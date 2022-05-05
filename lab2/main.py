e4c09c55edd31bc7e54d168310879a0dde895952
Alikyshkin <Alikyshkin@gmail.com>
2022-05-05 23:27:58 +0300

    fix

--- lab2/main.py
+++ lab2/main.py
@@ -1,19 +1,19 @@
 from random import *
-from numdifftools import Gradient
 import numpy as np
 import matplotlib.pyplot as plt
 import min_search as ms
 
-
-# метод наискорейшего спуска
-def steepest_descent_gradient(f, grad, x, eps, min_f, n=100):
+# Метод градиентного спуска
+def descent_gradient(f, grad, x, eps, min_f, n=100):
     steps = 0
     points = [np.copy(x)]
+    step = 1
     for _ in range(n):
         steps += 1
         gr = grad(x)
         g = lambda l_: f(x - gr * l_)
-        l = min_f(g, -10, 10, eps)
+        l = min_f(g, eps, step)
+        step = l
         x = x - gr * l
         points.append(np.copy(x))
         if np.linalg.norm(gr) < eps:
@@ -21,46 +21,19 @@ def steepest_descent_gradient(f, grad, x, eps, min_f, n=100):
     return x, steps, points
 
 
-# метод градиентного спуска
-def descent_gradient(grad, v, k, eps, n=100):
-    steps = 0
-    points = [np.copy(v)]
-    for i in range(1, n + 1):
-        steps += 1
-        l = 1 / min(i + 3, k + 3)
-        v = v - grad(v) * l
-        points.append(np.copy(v))
-        if np.linalg.norm(grad(v)) < eps:
-            break
-    return v, steps, points
-
-
-# метод ньютона
-def newton(grad, hesse, v, eps, n=100):
-    steps = 0
-    points = [np.copy(v)]
-    for _ in range(n):
-        steps += 1
-        g = -1 * grad(v)
-        h = hesse(v)
-        s = np.linalg.solve(h, g)
-        v += s
-        points.append(np.copy(v))
-        if np.linalg.norm(s) <= eps:
-            return v, steps, points
-
-
-# метод сопряженных градиентов
+# Метод сопряженных градиентов
 def conjugate_gradient(f, grad, x0, eps, min_f, n=100):
     k = 0
     xk = x0
     pk = -grad(x0)
     steps = 0
     points = [np.copy(x0)]
+    step = 0.7
     while True:
         steps += 1
-        alpha = min_f(lambda a: f(xk + a * pk), 0, 1, eps)
+        alpha = min_f(lambda a: f(xk + a * pk), eps, step)
         xk1 = xk + alpha * pk
+        step = alpha
         points.append(np.copy(xk1))
         if np.linalg.norm(grad(xk1)) < eps:
             return xk1, steps, points
@@ -75,30 +48,7 @@ def conjugate_gradient(f, grad, x0, eps, min_f, n=100):
         xk = xk1
 
 
-# метод сопряженных направлений
-def conugate_directions(f, grad, x, eps, min_f):  # Пауэлл
-    n = len(x)
-    D = np.eye(n)
-    steps = 0
-    points = [np.copy(x)]
-    while True:
-        steps += 1
-        r = min_f(lambda l: f(x + l * D[:, n - 1]), -10, 10, eps)
-        x = x + r * D[:, n - 1]
-        points.append(np.copy(x))
-        y = x
-        for i in range(n):
-            r = min_f(lambda l: f(x + l * D[:, i]), -10, 10, eps)
-            x = x + r * D[:, i]
-        for i in range(n - 1):
-            D[:, i] = D[:, i + 1]
-        D[:, n - 1] = y - x
-
-        if np.linalg.norm(D[:, n - 1]) < eps:
-            return x, steps, points
-
-
-# для графика поле
+# Для графика поле
 def make_field(f):
     x = np.linspace(-15, 15, 100)
     y = np.linspace(-15, 15, 100)
@@ -108,6 +58,7 @@ def make_field(f):
     return X, Y, Z
 
 
+# Рисуем график
 def draw_level_lines(func, points: list):
     x, y, z = make_field(func)
     points_x, points_y = [], []
@@ -128,96 +79,39 @@ def do_report(func, t):
     draw_level_lines(func, points)
 
 
-def generate_matrix(n, k):
-    matrix = [[0] * n for _ in range(n)]
-    for i in range(n):
-        matrix[i][i] = randint(1, k)
-    i = randint(0, n - 1)
-    matrix[i][i] = k
-    j = i
-    while j == i:
-        j = randint(0, n - 1)
-    matrix[j][j] = 1
-    return matrix
-
-
-def generate_function(n, k):
-    matrix = generate_matrix(n, k)
-    b = [randint(0, 100) for _ in range(n)]
-    func = lambda x: sum(x[i] ** 2 * matrix[i][i] - b[i] * x[i] for i in range(n))
-    return func
-
-
-# траектория реализованных методов (линия шагов оптимизации)
-def n_by_k(n):
-    fig, ax = plt.subplots()
-    s = [0] * 10000
-    for j in range(2, 5):
-        func = generate_function(j * 10, n)
-        grad = Gradient(func)
-        start = np.array([randint(-10000, 10000) for _ in range(j * 10)])
-        ans, steps, points = steepest_descent_gradient(func, grad, start, 0.001, ms.fibonacci, 10000)
-        s[j] = steps
-        print(f"{j * 10} j*10 = {steps} шагов") #эту функцию понять и простить
-
-    y = [0]
-    x = [0]
-    ax.plot(x, y, color='red')
-    for i in range(2, 4):
-        y = [s[i], s[i + 1]]
-        x = [i * 10, ((i + 1) * 10)]
-        ax.plot(x, y, color='red')
-    plt.show()
-
-
-def test_on(func, grad, hesse, x0, eps, min_f, ans):
-    print(f"Все ответы для x0 = {x0}, eps = {eps},  ans = {ans}")
-    print(f"Метод наискорейшего спуска = ", end="")
-    do_report(func, steepest_descent_gradient(func, grad, np.copy(x0), eps, min_f))
+def test_on(func, grad, x0, eps, min_f):
+    print(f"Все ответы для x0 = {x0}, eps = {eps}")
     print(f"Метод сопряженных градиентов = ", end="")
     do_report(func, conjugate_gradient(func, grad, np.copy(x0), eps, min_f))
-    # print(f"pavel_method = ", end="")
-    print(f"Метод сопряженных направлений = ", end="")
-    do_report(func, conugate_directions(func, grad, np.copy(x0), eps, min_f))
     print(f"Метод градиентного спуска = ", end="")
-    do_report(func, descent_gradient(grad, np.copy(x0), 20, eps))
-    print(f"Метод ньютона = ", end="")
-    do_report(func, newton(grad, hesse, np.copy(x0), eps))
-    print(f"траектория реализованных методов: \n ", end="")
-    n_by_k(10)
+    do_report(func, descent_gradient(func, grad, np.copy(x0), eps, min_f))
 
 
 def main():
     # f = x^3 * sin(x)
     # f = x^2
     # функции которые мы придумали
+    # x[0] = x, x[1] = y
     def func(x):
         return 4 * x[0] ** 2 + 2 * x[1] ** 2 + 3 * x[0] * x[1] + 2 * x[0] + 2 * x[1]
-        return x[0] ** 2 + x[1] ** 2
-        return (x[0] + 1) ** 2 + x[1] ** 2
-        return 6 * x[0] ** 2 - 4 * x[0] * x[1] + 3 * x[1] ** 2 + 4 * sqrt(5) * (x[0] + 2 * x[1]) + 22
-        return (x[0]) ** 2 + x[1] ** 2 + x[0] * x[1]
-        return (x[0] - 3) ** 2
-        return x ** 3 * sin(x)
+        # return x[0] ** 2 + x[1] ** 2 + x[0] * x[1] + 10 * x[0] - 10 * x[1] + 10
+        # return 3 * x[0] ** 2 + x[1] ** 2 - x[0] * x[1] - 4 * x[0]
 
-    # d/dx(x^3 sin(x)) = x^2 (3 sin(x) + x cos(x))
     def grad(x):
         return np.array([8 * x[0] + 3 * x[1] + 2, 3 * x[0] + 4 * x[1] + 2])
-        return np.array([2 * x[0], 2 * x[1]])
-        return np.array([12 * x[0] - 4 * x[1] + 4 * sqrt(5), -4 * x[0] + 6 * x[1] - 8 * sqrt(5)])
-        return np.array([2 * x[0] + x[1], 2 * x[1] + x[0]])
-        return x ** 2 * 3 * sin(x) + x ** 3 * cos(x)
+        # return np.array([2 * x[0] + x[1] + 10, x[0] + 2 * (x[1] - 5)])
+        # return np.array([6 * x[0] - x[1] - 4, 2 * x[1] - x[0]])
 
-    # d^2/dx^2(x^3 sin(x)) = x (6 x cos(x) - (x^2 - 6) sin(x))
-    def hesse(x):
-        return np.array([[8, 3], [3, 4]])
-        return np.array([[2, 0], [0, 2]])
-        return np.array([[2, 1], [1, 2]])
-        return np.array([[2]])
-        return np.array([[12, -4], [-4, 6]])  # only one variable
+    print(f"\n==Метод с постоянным шагом==\n")
+    test_on(func, grad, np.array([15.0, 10.0]), 0.001, ms.const)
+    print(f"\n==Метод дробления==\n")
+    test_on(func, grad, np.array([15.0, 10.0]), 0.001, ms.step_decreaser)
+    print(f"\n==Метод линейного поиска==\n")
+    test_on(func, grad, np.array([15.0, 10.0]), 0.001, ms.linear_search)
+    print(f"\n==Метод золотого сечения==\n")
+    test_on(func, grad, np.array([15.0, 10.0]), 0.1, ms.gold_section)
+    print(f"\n==Метод Фибоначчи==\n")
+    test_on(func, grad, np.array([15.0, 10.0]), 0.001, ms.fibonacci)
 
-    test_on(func, grad, hesse, np.array([15.0, 10.0]), 0.001, ms.fibonacci, [-0.08695585, -0.43478307])
 
-
-if __name__ == '__main__':
-    main()
+main()
--- lab2/min_search.py
+++ lab2/min_search.py
@@ -2,69 +2,11 @@ import numpy as np
 from scipy.misc import derivative
 
 
-# def const():
-#     return 0.3
+# Постоянная величина шага
+def const(f, eps, step):
+    return 0.1
 
-
-def fibonacci(f, x_start, x_end, eps):
-    fibonacciSeries = [1, 1]
-    function_calcs = 0
-
-    while 1:
-        newItem = fibonacciSeries[len(fibonacciSeries) - 1] + fibonacciSeries[len(fibonacciSeries) - 2]
-        fibonacciSeries.append(newItem)
-        if newItem > abs(x_end - x_start) / eps:
-            break
-
-    n = len(fibonacciSeries) - 1
-    x1 = x_start + (fibonacciSeries[n - 2] / fibonacciSeries[n]) * (x_end - x_start)
-    x2 = x_start + (fibonacciSeries[n - 1] / fibonacciSeries[n]) * (x_end - x_start)
-    fx1 = f(x1)
-    fx2 = f(x2)
-    function_calcs += 2
-
-    iters = 0
-    while n > 2:
-        # print("Iter:", iters, ", Curr interval: (", x_start, ", ", x_end, "), diff between prev interval: ", (abs(x_end - x_start)), ", xmin: ", (x_start + x_end)/2.0, ", func calculations: ", function_calcs)
-        n -= 1
-        iters += 1
-
-        if fx1 < fx2:
-            x_end = x2
-            x2 = x1
-            fx2 = fx1
-            x1 = x_start + ((fibonacciSeries[n - 2] / fibonacciSeries[n]) * (x_end - x_start))
-            fx1 = f(x1)
-            function_calcs += 1
-
-        else:
-            x_start = x1
-            x1 = x2
-            fx1 = fx2
-            x2 = x_start + (fibonacciSeries[n - 1] / fibonacciSeries[n]) * (x_end - x_start)
-            fx2 = f(x2)
-            function_calcs += 1
-
-    res = x_start + x_end / 2
-    return res
-
-
-# Альфа - коэффициент масштаба для направления
-def linear_search(f, eps):
-    alpha = 0
-    der = derivative(f, alpha)
-    der_test = derivative(f, alpha + eps / 2)
-    if (der >= 0 and der - der_test > 0) or (der < 0 and der - der_test < 0):
-        while der * derivative(f, alpha) > 0:
-            alpha += eps / 2
-    else:
-        while der * derivative(f, alpha) > 0:
-            alpha -= eps / 2
-
-    return alpha
-
-
-def step_drill(f, step):
+def step_decreaser(f, e, step):
     v = 0.4
     der = 0
     i = 0
@@ -74,27 +16,81 @@ def step_drill(f, step):
     return step
 
 
-def gold_section(f, eps):
+# Метод золотого сечения
+def gold_section(f, eps, step):
     a = 0
-    b = 1
+    b = 10
     phi = (1 + np.sqrt(5)) / 2
     rphi = 2 - phi
-    x1 = a + rphi * (b - a)
-    x2 = b - rphi * (b - a)
+    x1 = a + rphi * abs(b - a)
+    x2 = b - rphi * abs(b - a)
     f1 = f(x1)
     f2 = f(x2)
-    while np.fabs(a - b) > eps:
+    while abs(a - b) > eps:
         if f1 < f2:
             b = x2
             x2 = x1
             f2 = f1
-            x1 = a + rphi * (b - a)
+            x1 = a + rphi * abs(b - a)
             f1 = f(x1)
         else:
             a = x1
             x1 = x2
             f1 = f2
-            x2 = b - rphi * (b - a)
+            x2 = b - rphi * abs(b - a)
             f2 = f(x2)
-    res = x1 + x2 / 2
-    return res
+    step = x1 + x2 / 2
+    return step
+
+
+def fib(n):
+    fib1 = fib2 = 1
+    n = int(n) - 2
+    while n > 0:
+        fib1, fib2 = fib2, fib1 + fib2
+        n -= 1
+    return fib2
+
+
+def fibonacci(f, eps, step):
+    a = 0
+    b = 10
+    n = 0
+    while fib(n) < (b - a) / (2 * eps):
+        n += 1
+    l = a + (fib(n - 2) / fib(n)) * (b - a)
+    m = a + (fib(n - 1) / fib(n)) * (b - a)
+    yL = f(l)
+    yM = f(m)
+    for k in range(1, n - 1):
+        if yL > yM:
+            a = l
+            l = m
+            m = a + (fib(n - k - 1) / fib(n - k)) * (b - a)
+            yM = f(m)
+            yL = f(l)
+        else:
+            b = m
+            m = l
+            l = a + (fib(n - k - 2) / fib(n - k)) * (b - a)
+            yL = f(l)
+            yM = f(m)
+    m = l + eps
+    if f(l) <= f(m):
+        a = l
+    else:
+        b = m
+    return a + b / 2
+
+def linear_search(f, e, step):
+    alpha = 0
+    der = derivative(f, alpha)
+    der_test = derivative(f, alpha + e / 2)
+    if (der >= 0 and der - der_test > 0) or (der < 0 and der - der_test < 0):
+        while der * derivative(f, alpha) > 0:
+            alpha += e / 2
+    else:
+        while der * derivative(f, alpha) > 0:
+            alpha -= e / 2
+
+    return alpha
